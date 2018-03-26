a33ce046a3060beb6cf92253bd9d726c49a542e9
Yangchen Shen <shyangch@gmail.com>
2018-03-26 16:03:42 -0700

    Refactor root to include store

--- todo_list/frontend/root.jsx
+++ todo_list/frontend/root.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
+import App from './components/app.jsx';
+import { Provider } from 'react-redux';
 
-class Root extends React.Component {
+const Root = ({store}) => (
+  <Provider store={ store }>
+    <App />
+  </Provider>
+);
 
-  constructor () {
-    super();
-  }
-  
-  render () {
-    return (
-      <div className="todo-Root">
-        <h1>Todos App</h1>
-      </div>
-    );
-  }
-}
+// alternative syntax:
+// const Root = ({store}) => {
+//   console.dir(store);
+//   return (
+//   <Provider store={ store }>
+//     <App />
+//   </Provider>
+// );
+// };
+
+// class Root extends React.Component {
+//
+//   render () {
+//     return (
+//       <div className="todo-Root">
+//         <h1>Todos App</h1>
+//         <App />
+//       </div>
+//     );
+//   }
+// }
 
 export default Root;
--- todo_list/frontend/todo_redux.jsx
+++ todo_list/frontend/todo_redux.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Root from './root';
-import store from './store/store';
+import configureStore from './store/store';
 import { receiveTodos, receiveTodo } from './actions/todo_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  window.store = store();
+  const store = configureStore();
+  window.store = configureStore();
   window.receiveTodo = receiveTodo;
   window.receiveTodos = receiveTodos;
-  ReactDom.render(<Root store={store}/>, root);
+  ReactDom.render(<Root store={ store } />, root);
 });
