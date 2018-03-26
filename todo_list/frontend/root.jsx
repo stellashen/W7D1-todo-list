041b2347dfba557e74dc835771b25e608f3bcae7
Yangchen Shen <shyangch@gmail.com>
2018-03-26 11:59:47 -0700

    Initial commit, set up file tree

---
+++ todo_list/frontend/root.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Root extends React.Component {
+
+  constructor () {
+    super();
+  }
+  
+  render () {
+    return (
+      <div className="todo-Root">
+        <h1>Todos App</h1>
+      </div>
+    );
+  }
+}
+
+export default Root;
