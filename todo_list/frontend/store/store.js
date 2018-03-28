4d8a3261e3b503450ffb6fbeccab38c5d925da14
Yangchen Shen <shyangch@gmail.com>
2018-03-27 18:48:12 -0700

    add redux devtools extension to store

--- todo_list/frontend/store/store.js
+++ todo_list/frontend/store/store.js
@@ -3,7 +3,8 @@ import rootReducer from '../reducers/root_reducer';
 import logger from 'redux-logger';
 
 const configureStore = (preloadedState= {} ) => {
-  const store = createStore(rootReducer, preloadedState);
+  const store = createStore(rootReducer, preloadedState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
   return store;
 };
 
